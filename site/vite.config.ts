c9396f679b6ad4ce5e0a7c5c96ad70c96c705acf
Hariharan <14965074+cvhariharan@users.noreply.github.com>
2026-05-18 10:54:35 +0530

    chore: upgrade vite-plugin-svelte

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   cacheDir: "node_modules/.vite",
   build: {
     cssCodeSplit: true,
-    minify: 'esbuild',
+    minify: true,
   },
   optimizeDeps: {
     force: false,
